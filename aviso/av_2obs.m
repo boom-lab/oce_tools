d91179faa7c74d9794802c87c5615b13d0ea32f4
dnicholson <whoiboomlab@gmail.com>
2015-11-11 14:11:05 -0500

    updated aviso access
    need to merge to master branch

---
+++ aviso/av_2obs.m
@@ -0,0 +1,96 @@
+function [ outvar,latout,lonout,tout] = av_2obs(lat,lon,t,var,varargin)
+% hy_2obs
+% -------------------------------------------------------------------------
+% extracts aviso output nearest to inputted lat/lon/t vectors
+% 
+% -------------------------------------------------------------------------
+% USAGE:
+% -------------------------------------------------------------------------
+% [ssh] = av_2obs(lat,lon,t,'sla');
+% -------------------------------------------------------------------------
+% INPUTS:
+% -------------------------------------------------------------------------
+% lat:      vector of observed latitudes
+% lon:      vector of observed longitudes (between [-180 and +360])
+% t:        datetime or datenum time input - vector or scalar
+% var:      string of input variable
+% varargin: optional variables passed through to av_url.m
+% 
+% -------------------------------------------------------------------------
+% OUTPUTS:
+% -------------------------------------------------------------------------
+% outvar:   vector of requested data
+% units:    string with units for outvar
+% fname:    opendap addresses for files that were accesed
+% lonout:   vector of longitudes corresponding to matched NASA grid cell
+% latout:   vector of latitudes corresponding to matched NASA grid cell
+%
+%
+% -------------------------------------------------------------------------
+% ALSO SEE: 
+% -------------------------------------------------------------------------
+% av_url.m
+% av_slab.m
+%
+% -------------------------------------------------------------------------
+% ABOUT: David Nicholson // dnicholson@whoi.edu // 9 NOV 2015
+% -------------------------------------------------------------------------
+
+nobs = length(lat);
+% check if t is scalar and resize to match lat and lon
+if length(t) == 1
+    t = repmat(t,nobs,1);
+end
+
+if ~isdatetime(t)
+    dtm = datetime(t, 'ConvertFrom', 'datenum');
+else
+    dtm = t;
+end
+dtm = dateshift(dtm,'start','day');
+
+if length(lon) ~= nobs || length(t) ~= nobs
+    error('lat,lon and t must be the same length or scalar');
+end
+
+lon = mod(lon,360);
+% clean up lon, t and construct full filename      
+
+latrng = [floor(min(lat)) ceil(max(lat))];
+% 0 - 360 range
+lonrng1 = [floor(min(lon)) ceil(max(lon))];
+lon2 = lon;
+lon2(lon > 180) = lon(lon > 180) - 360;
+lonrng2 = [floor(min(lon2)) ceil(max(lon2))];
+if diff(lonrng1) < diff(lonrng2)
+    % within 0 - 360 range
+    lonrng = lonrng1;
+else
+    % wrap around prime meridian
+    lonrng = flip(lonrng1);
+end
+trng = [min(dtm) max(dtm)];
+
+[slab,latAv,lonAv,tAv] = av_slab(latrng,lonrng,trng,var);
+
+
+%% Initialize output and get nearest datapoint to each obs point
+NV = nan(nobs,1);
+outvar = NV;
+latout = NV;
+lonout = NV;
+tout = datetime(NV,'ConvertFrom','datenum');
+
+for ii = 1:nobs
+    [~,ilat] = min(abs(lat(ii) - latAv));
+    [~,ilon] = min(abs(lon(ii) - lonAv));
+    [~,it] = min(abs(tAv-dtm(ii)));
+    outvar(ii) = slab(ilat,ilon,it);
+    latout(ii) = latAv(ilat);
+    lonout(ii) = lonAv(ilon);
+    tout(ii) = tAv(it);
+end
+
+
+end
+
---
+++ aviso/av_slab.m
@@ -0,0 +1,126 @@
+function [ slab,lat,lon,t] = av_slab(latRng,lonRng,tRng,varName,varargin)
+
+% av_slab
+% -------------------------------------------------------------------------
+% accesses aviso thredds server to retrieve slab of data that can wrap 
+% around 360/0 longitude edge
+% -------------------------------------------------------------------------
+% USAGE:
+% -------------------------------------------------------------------------
+% [ssh, lath, lonh] = av_slab(latrng,lonrng,NaN,[t,t],'surf_el');
+%
+% -------------------------------------------------------------------------
+% INPUTS:
+% -------------------------------------------------------------------------
+% Required
+% lonRng:   range of longitude to retrieve [W_edge E_edge]
+% latRng:   range of latitude to retrieve [S_edge N_edge]
+% zRng:     depth range to achieve (positive down) [shallow_edge deep_egdge]
+% tRng:     datenum or datetime range to retrieve - 
+% varName:  string name of variable
+%
+% Optional
+% delayMode: option for delayed time ('dt') or near-realtime ('nrt') [default]
+%
+% -------------------------------------------------------------------------
+% OUTPUTS:
+% -------------------------------------------------------------------------
+% url:  full opendap/thredds address of requested file
+%
+% -------------------------------------------------------------------------
+% ABOUT:  David Nicholson // dnicholson@whoi.edu // 8 NOV 2015
+% -------------------------------------------------------------------------
+%%% parse input parameters
+p = inputParser;
+
+expectedVarName = {'sla','madt','u','v','uv','uwind','vwind','mwind','mswh'};
+defaultDT = 'nrt';
+expectedDT = {'dt','nrt'};
+
+addRequired(p,'latRng',@(x) isnumeric(x) & length(x) == 2);
+addRequired(p,'lonRng',@(x) isnumeric(x) & length(x) == 2);
+addRequired(p,'tRng',@(x) (isnumeric(x) || isdatetime(x)) & length(x) == 2);
+addRequired(p,'varName',@(x) any(validatestring(x,expectedVarName)));
+addParameter(p,'delayMode',defaultDT,@(x) any(validatestring(x,expectedDT)));
+
+parse(p,latRng,lonRng,tRng,varName,varargin{:});
+inputs = p.Results;
+
+% validated inputs
+lonRng = inputs.lonRng;
+latRng = inputs.latRng;
+tRng = inputs.tRng;
+varName = inputs.varName;
+delayMode = inputs.delayMode;
+
+if ~isdatetime(tRng)
+    tRng = datetime(tRng, 'ConvertFrom', 'datenum','TimeZone','UTC');
+end
+tRng = dateshift(tRng,'start','day','nearest');
+
+% if tRng(1) < datetime('now','TimeZone','UTC')
+%     url1 = av_url(varn,'delayMode','dt');
+% else
+%     url1 = [];
+% end
+% 
+% if tRng(2) > datetime('now','TimeZone','UTC')-calyears(1);
+%     url2 = av_url(varn,'delayMode','dt');
+% else
+%     url2 = [];
+% end
+    
+% read url for start and end times
+url = av_url(varName,'delayMode',delayMode);
+
+avlon = ncread(url,'lon');
+avlat = ncread(url,'lat');
+
+% get requested lat range
+ilat = find(avlat >= latRng(1) & avlat <= latRng(2));
+nlat = length(ilat);
+lat = double(avlat(ilat));
+
+% check if tRng is entirely within one experiment, or if it bridges two
+avt = ncread(url,'time');
+% aviso time is days since 01 JAN 1950
+avdtm = datetime(1950,1,1+avt);
+it = find(avdtm <= tRng(2) & avdtm >= tRng(1));
+nt = length(it);
+t = avdtm(it);
+
+if isempty(it)
+    [mint,it] = min(abs(avdtm-tRng(1)));
+    if mint > 3
+        error('no data found within 3 days')
+    end
+end
+
+% lon is on 0 to 360 scale
+lonRng = mod(lonRng,360);
+avlon = mod(avlon,360);
+
+if lonRng(1) > lonRng(2)
+    ilon1 = find(avlon >= lonRng(1));
+    nlon1 = length(ilon1);
+    ilon2 = find(avlon <= lonRng(2));
+    nlon2 = length(ilon2);
+    lon = [avlon(ilon1);avlon(ilon2)];
+    slab = nan(nlon1+nlon2,nlat,nt);
+    slab(1:nlon1,:,:) = ncread(url,varName,[ilon1(1),ilat(1),it(1)],...
+        [nlon1,nlat,nt]);
+    slab(nlon1+1:end,:,:) = ncread(url,varName,[1,ilat(1),it(1)],...
+        [nlon2,nlat,nt]);
+else
+    ilon = find(avlon >= lonRng(1) & avlon <= lonRng(2));
+    lon = avlon(ilon);
+    nlon = length(lon);
+    slab = ncread(url,varName,[ilon(1),ilat(1),it(1)],[nlon,nlat,nt]);
+end
+
+% change to lat, lon, t
+slab = double(permute(slab,[2,1,3]));
+lon = double(lon);
+
+end
+
